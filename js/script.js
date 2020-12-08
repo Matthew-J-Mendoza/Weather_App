9aebf9fe9c367df46f5ea42b3df13a835e97f01e
Matthew-J-Mendoza <mattjm112999@gmail.com>
2020-12-08 15:43:35 -0800

    more details added

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ let apikey = '&appid=1de51df452c6c848122c0ddbab965dc4';
 
 let fiveDayUrl = "http://api.openweathermap.org/data/2.5/forecast?q=";
 
+let searchBTN = document.getElementById('searchBTN');
+let searchBar = document.getElementById('searchBar');
+
 // async function LW3(url){
 //     let w = await fetch(url);
 //     let d = await w.json();
@@ -15,11 +18,22 @@ let fiveDayUrl = "http://api.openweathermap.org/data/2.5/forecast?q=";
 // LW3(fiveDayUrl+City+apikey);
 
 async function LW3(url){
+    
     let Forecast = await fetch(url);
     let forecastData = await Forecast.json();
-    console.log(forecastData);
-    console.log(forecastData.name);
+    if(forecastData.cod != 404){
+        console.log(forecastData);
+        console.log(forecastData.name);
+    } else{
+        alert ("City not found")
+    }
 }
 
 
-LW5(fiveDayUrl+City+apikey);
+// LW5(fiveDayUrl+City+apikey);
+
+searchBTN.addEventListener('click', function(e){
+    console.log(searchBar.value);
+    LW3(url_pt1+searchBar.value+apikey);
+})
+
